6e85055c8b55532da0e375714c8c1c18bd72366f
rutanshuj <rjhaveri41@gmail.com>
2018-05-06 17:56:28 +0530

    anomaly issue, image binarization using decode online tool

--- scripts/finalCaptchaBreaker.py
+++ scripts/finalCaptchaBreaker.py
@@ -3,13 +3,15 @@ import numpy as np
 from PIL import Image
 import os
 import pytesseract
+import matplotlib.cm as cm
+import matplotlib.pyplot as plt
 
 class preProcessing:
 
     def pre_proc_image(self,img):
         img = np.array(img,dtype=np.uint8)
         retvalue, img = cv2.threshold(img, 4, 255, cv2.THRESH_BINARY)
-        # cv2.imshow("Threshold Changes",img)
+        # cv2.imshow("Threshold Changes",np.array(img,dtype=np.uint8))
         # cv2.waitKey(0)
         img = Image.fromarray(img)
         img_removed_noise = self.apply_median_filter(img)
@@ -19,6 +21,7 @@ class preProcessing:
         img_gray = img.convert('L')
         img_gray = cv2.medianBlur(np.asarray(img_gray), 3)
         img_bw = (img_gray > np.mean(img_gray)) * 255
+
         return img_bw
 
 if __name__ == '__main__':
@@ -28,8 +31,8 @@ if __name__ == '__main__':
     imgOP = p.pre_proc_image(image)
 
     filename = "Output.png".format(os.getpid())
-    cv2.imwrite(filename, np.array(imgOP))
-
+    cv2.imwrite(filename, imgOP)
     text = pytesseract.image_to_string(Image.open(filename))
+
     #os.remove(filename)
     print(text)
